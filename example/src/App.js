757601e5e67cc4dbec1199b65ad8ea4b8238ee2a
MansurNurani <mansur.nurani@gmail.com>
2021-09-12 18:34:51 +0430

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-cmp-2021'
+import 'react-cmp-2021/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
